5da24398b3f9186ae519a83074974419fb4559f1
Silvano Cirujano Cuesta <silvanociru@gmx.net>
2011-07-04 08:55:25 +0200

    First version of db_backend and corresponding tests

--- db_backend.py
+++ db_backend.py
@@ -1,5 +1,4 @@
-'''
-'''
+'handles the database where the multimedia items are stored'
 
 import os
 import sqlite3
@@ -24,22 +23,24 @@ SCHEMA_FILES = 'CREATE TABLE files (' + \
 									'path TEXT UNIQUE, ' + \
 									'file_checksum TEXT, ' + \
 									'content_checksum TEXT, ' + \
-									'tags INTEGER, ' + \
-									'FOREIGN KEY(tags) REFERENCES tags(rowid));'
-SCHEMA_ITEMS = 'CREATE TABLE items (' + \
+									'tags REFERENCES tags(tags_id));'
+SCHEMA_ITEMS = 'CREATE TABLE simple_items (' + \
 									'item_id INTEGER PRIMARY KEY ASC, ' + \
 									'name TEXT UNIQUE, ' + \
-									'content_file INTEGER, ' + \
-									'tags_file INTEGER, ' + \
-									'FOREIGN KEY(content_file) REFERENCES files(rowid), ' + \
-									'FOREIGN KEY(tags_file) REFERENCES files(rowid));'
+									'content_file REFERENCES files(file_id), ' + \
+									'tags_file REFERENCES files(file_id));'
 SCHEMA_OTHER_FILES = 'CREATE TABLE other_files(' + \
-									'file INTEGER PRIMARY KEY ASC, ' + \
-									'item INTEGER, ' + \
-									'FOREIGN KEY(file) REFERENCES files(rowid), ' + \
-									'FOREIGN KEY(item) REFERENCES items(rowid));'
+									'file REFERENCES files(file_id), ' + \
+									'item REFERENCES simple_items(item_id));'
 SCHEMA_FILE_INDEX = 'CREATE INDEX file_path ON files(path);'
-SCHEMA_ITEM_INDEX = 'CREATE INDEX item_name ON items(name);'
+SCHEMA_ITEM_INDEX = 'CREATE INDEX item_name ON simple_items(name);'
+SCHEMA_ITEMS_VIEW = 'CREATE VIEW items AS ' + \
+									'SELECT i.name, cf.path, tf.path, group_concat(ef.path,"|") ' + \
+									'FROM simple_items i, files cf, files tf, files ef, other_files of ' + \
+									'WHERE i.content_file = cf.file_id AND ' + \
+									'i.tags_file = tf.file_id AND ' + \
+									'i.item_id = of.item AND ' + \
+									'of.file = ef.file_id;'
 
 class DbConnector:
 	def __init__(self, db_path=''):
@@ -52,13 +53,14 @@ class DbConnector:
 		# if database doesn't have the tables yet, create them
 		self._db_curs.execute("SELECT name FROM sqlite_master WHERE type='table';")
 		tables = self._db_curs.fetchall()
-		if (not (u'files',) in tables) or (not (u'items',) in tables) or (not (u'tags',) in tables):
+		if (not (u'files',) in tables) or (not (u'simple_items',) in tables) or (not (u'tags',) in tables):
 			self._db_curs.execute(SCHEMA_TAGS)
 			self._db_curs.execute(SCHEMA_FILES)
 			self._db_curs.execute(SCHEMA_ITEMS)
 			self._db_curs.execute(SCHEMA_OTHER_FILES)
 			self._db_curs.execute(SCHEMA_FILE_INDEX)
 			self._db_curs.execute(SCHEMA_ITEM_INDEX)
+			self._db_curs.execute(SCHEMA_ITEMS_VIEW)
 			self._photos_db.commit()
 	def __del__(self):
 		'closes the connection to the DB before destroying the object'
@@ -104,7 +106,7 @@ class DbConnector:
 	def add_item(self, name):
 		'adds a multimedia item to the DB'
 		cur = self._db_curs
-		self._db_curs.execute('INSERT INTO items (name) VALUES (?);', [name])
+		self._db_curs.execute('INSERT INTO simple_items (name) VALUES (?);', [name])
 		self._photos_db.commit()
 		return self._db_curs.lastrowid
 
@@ -116,7 +118,7 @@ class DbConnector:
 			content_file_id = cur.fetchone()[0]
 		except TypeError:
 			raise IndexError, "trying to associate the non-existing file %s to the item %s!" % (content_file, name)
-		self._db_curs.execute('UPDATE items SET content_file = ? WHERE name = ?;', (content_file_id, name))
+		self._db_curs.execute('UPDATE simple_items SET content_file = ? WHERE name = ?;', (content_file_id, name))
 		self._photos_db.commit()
 		return self._db_curs.lastrowid
 
@@ -128,14 +130,16 @@ class DbConnector:
 			tags_file_id = cur.fetchone()[0]
 		except TypeError:
 			raise IndexError, "trying to associate the non-existing file %s to the item %s!" % (tags_file, name)
-		self._db_curs.execute('UPDATE items SET tags_file = ? WHERE name = ?;', (tags_file_id, name))
+		self._db_curs.execute('UPDATE simple_items SET tags_file = ? WHERE name = ?;', (tags_file_id, name))
 		self._photos_db.commit()
 		return self._db_curs.lastrowid
 
 	def get_item(self, item_name):
-		self._db_curs.execute('SELECT * FROM items WHERE name=?', (item_name,))
-		result = self._db_curs.fetchone()
-		return CameraItem(item_name, content_file = result[1], tags_file = result[2])
+		'returns the specified item'
+		self._db_curs.execute('SELECT * FROM items WHERE name = ?;', [item_name])
+		(item_name, content_file, tags_file, extra_files) = self._db_curs.fetchone()
+		extra_files = extra_files.split('|')
+		return (item_name, content_file, tags_file, extra_files)
 
 	def add_extra_file(self, file_path, item_name):
 		'adds a relationship with an extra file to the DB'
@@ -144,36 +148,15 @@ class DbConnector:
 		try:
 			file_id = cur.fetchone()[0]
 		except TypeError:
-			raise IndexError, "trying to a associate the unknown file %s to the item %s in an 'other file' relationship!" % (file_path, item_name)
-		cur.execute('SELECT item_id FROM items WHERE name = "%s";' % item_name)
+			raise IndexError, "trying to a associate the unknown file %s to the ' + \
+				'item %s in an 'other file' relationship!" % (file_path, item_name)
+		cur.execute('SELECT item_id FROM simple_items WHERE name = "%s";' % item_name)
 		try:
 			item_id = cur.fetchone()[0]
 		except TypeError:
-			raise IndexError, "trying to a associate the file %s to the unknown item %s in an 'other file' relationship!" % (file_path, item_name)
+			raise IndexError, "trying to a associate the file %s to the unknown ' + \
+				'item %s in an 'other file' relationship!" % (file_path, item_name)
 		cur.execute('INSERT INTO other_files (file, item) ' + \
 						'VALUES (?, ?);', (file_id, item_id))
 		self._photos_db.commit()
 		return cur.lastrowid
-
-	def _add(self, file_path):
-		'adds item to the list'
-		# get the name of the item
-		item_name = CameraItem.path_to_name(file_path)
-		if item_name == '':
-			trace_err('WARNING! trying to create an item with empty name?!')
-			return
-		# if the item already exists, get the item
-		item_attrs = self._get_item(item_name)
-		if item_attrs == None:
-			item = CameraItem(item_name)
-			self._db_curs.execute('INSERT INTO items VALUES (?,?,?)', (item_name, '', ''))
-			self._photos_db.commit()
-		else:
-			content_file = item_attrs[1]
-			tags_file = item_attrs[2]
-			item = CameraItem(item_name, content_file, tags_file)
-		# add the file to the item
-		item.add(file_path)
-		# example for updating file 'content_file' of item
-		self._db_curs.execute('UPDATE items SET content_file=? WHERE name=?', (content_file, item_name))
-		self._photos_db.commit()
--- tests.py
+++ tests.py
@@ -34,11 +34,18 @@ class TestDbBackend(unittest.TestCase):
 							'file_checksum': u'f1l3ch3cksum3',
 							'content_checksum': u'c0nt3ntch3cksum3'
 							}
+		self.test_file_poor_3 = {'path': u'/this/is/a/path_4',
+							'file_checksum': u'f1l3ch3cksum4',
+							'content_checksum': u'c0nt3ntch3cksum4'
+							}
 		self.test_item = {'name': u'/this/is/a/name', 
 							'content_file': self.test_file_poor_1['path'],
 							'tags_file': self.test_file_rich_1['path']
 							}
-		self.test_extra_file = {'file': self.test_file_poor_2['path'],
+		self.test_extra_file_1 = {'file': self.test_file_poor_2['path'],
+							'item': self.test_item['name']
+							}
+		self.test_extra_file_2 = {'file': self.test_file_poor_3['path'],
 							'item': self.test_item['name']
 							}
 
@@ -53,7 +60,7 @@ class TestDbBackend(unittest.TestCase):
 		cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
 		tables = cur.fetchall()
 		self.assertTrue(len(tables) == 4)
-		self.assertTrue((u'items',) in tables)
+		self.assertTrue((u'simple_items',) in tables)
 		self.assertTrue((u'tags',) in tables)
 		self.assertTrue((u'files',) in tables)
 		self.assertTrue((u'other_files',) in tables)
@@ -61,200 +68,85 @@ class TestDbBackend(unittest.TestCase):
 		db.close()
 		os.remove(self._db_path)
 
-	def _add_poor_file(self):
+	def test_add(self):
+		'test that the addition of files, items and extra files works properly'
+		return
+		# initialize the DB connection to check the addition
+		db = sqlite3.connect(self._db_path)
+		cur = db.cursor()
+		# test the addition of a poor file
 		test_file = self.test_file_poor_1
 		self._db.add_poor_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'])
-
-	def test_add_poor_file(self):
-		'test that the addition of files without metadata works properly'
-		# add a poor file
-		self._add_poor_file()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the file entry
-		for tag_name in self.test_file_poor_1.keys():
+		for tag_name in test_file.keys():
 			cur.execute("SELECT " + tag_name + " FROM files;")
 			tag = cur.fetchone()
-			self.assertTrue(tag == (self.test_file_poor_1[tag_name],))
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
-
-	def _add_tags(self):
-		test_tags = self.test_tags
-		self._db.add_tags(test_tags['model'], test_tags['software'], test_tags['date_time_original'],
-			test_tags['create_date'], test_tags['image_width'],	test_tags['image_height'],
-			test_tags['tags_list'],	test_tags['hierarchical_subject'], test_tags['subject'], test_tags['keywords'])
-
-	def test_add_tags(self):
-		'test that the addition of tags works properly'
-		# add the tags of a JPEG file
-		self._add_tags()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the tags entry
-		for tag_name in self.test_tags.keys():
-			cur.execute("SELECT " + tag_name + " FROM tags;")
-			tag = cur.fetchone()
-			self.assertTrue(tag == (self.test_tags[tag_name],))
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
-
-	def _add_rich_file(self):
+			self.assertTrue(tag == (test_file[tag_name],))
+		# test the addition of a rich file
 		test_file = self.test_file_rich_1
 		self._db.add_rich_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'],
 			test_file['tags'])
-
-	def test_add_rich_file(self):
-		'test that the addition of files with metadata works properly'
-		# add a rich file
-		self._add_rich_file()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the file entry and the corresponding tags entry
-		for tag_name in self.test_file_rich_1.keys():
+		for tag_name in test_file.keys():
 			if tag_name != 'tags':
 				cur.execute("SELECT " + tag_name + " FROM files;")
 				tag = cur.fetchone()
-				self.assertTrue(tag == (self.test_file_rich_1[tag_name],))
+				self.assertTrue(tag == (test_file[tag_name],))
 			else:
 				for tag_name in self.test_tags.keys():
 					cur.execute("SELECT " + tag_name + " FROM tags LEFT JOIN files;")
 					tag = cur.fetchone()
 					self.assertTrue(tag == (self.test_tags[tag_name],))
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
-
-	def _add_item(self):
+		# test the addition of an item
 		test_item = self.test_item
 		self._db.add_item(test_item['name'])
-
-	def test_add_item(self):
-		'test that the addition of items works properly'
-		# add the item
-		self._add_item()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the expected item
-		cur.execute("SELECT name FROM items;")
+		cur.execute("SELECT name FROM simple_items;")
 		values = cur.fetchall()
 		self.assertTrue(len(values) == 1)
-		self.assertTrue(values[0][0] == self.test_item['name'])
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
-
-	def _add_item_content(self):
-		test_item = self.test_item
-		self._add_poor_file()
-		self._db.add_item(test_item['name'])
+		self.assertTrue(values[0][0] == test_item['name'])
+		# test the addition of a content file to the item
 		self._db.add_item_content(test_item['name'], test_item['content_file'])
-
-	def test_add_item_content(self):
-		'test that the addition of a content file to an item works properly'
-		# add the item
-		self._add_item_content()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the expected item
-		cur.execute("SELECT name FROM items;")
-		values = cur.fetchall()
-		self.assertTrue(len(values) == 1)
-		self.assertTrue(values[0][0] == self.test_item['name'])
-		# check that content_file has been properly assigned
-		cur.execute("SELECT content_file FROM items;")
+		cur.execute("SELECT content_file FROM simple_items;")
 		value = cur.fetchone()[0]
 		cur.execute("SELECT path FROM files WHERE file_id = %d;" % value)
 		value = cur.fetchone()[0]
 		self.assertTrue(value == self.test_item['content_file'])
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
-
-	def _add_item_tags(self):
-		test_item = self.test_item
-		self._add_rich_file()
-		self._db.add_item(test_item['name'])
+		# test the addition of a tags file to the item
 		self._db.add_item_tags(test_item['name'], test_item['tags_file'])
-
-	def test_add_item_tags(self):
-		'test that the addition of a tags file to an item works properly'
-		# add the item
-		self._add_item_tags()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains the expected item
-		cur.execute("SELECT name FROM items;")
-		values = cur.fetchall()
-		self.assertTrue(len(values) == 1)
-		self.assertTrue(values[0][0] == self.test_item['name'])
-		# check that tags_file has been properly assigned
-		cur.execute("SELECT tags_file FROM items;")
+		cur.execute("SELECT tags_file FROM simple_items;")
 		value = cur.fetchone()[0]
 		cur.execute("SELECT path FROM files WHERE file_id = %d;" % value)
 		value = cur.fetchone()[0]
 		self.assertTrue(value == self.test_item['tags_file'])
+		# test the addition of an extra file to the item
+		test_file = self.test_file_poor_2
+		self._db.add_poor_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'])
+		self._db.add_extra_file(test_file['path'], test_item['name'])
 		# close DB connection and delete it
 		db.close()
 		os.remove(self._db_path)
-
-	def _add_full_item(self):
-		test_item = self.test_item
-		self._add_rich_file()
-		self._add_poor_file()
-		self._db.add_item(test_item['name'])
-		self._db.add_item_content(test_item['name'], test_item['content_file'])
-		self._db.add_item_tags(test_item['name'], test_item['tags_file'])
 
 	def test_get_item(self):
 		'test that getting an item works'
-		return
-		#add the item
-		self._add_full_item()
-		#get the item
-		item = self._db.get_item(test_item['name'])
-
-	def _add_extra_file(self):
-		self._add_item()
-		test_file = self.test_file_poor_2
+		# add the item
+		test_file = self.test_file_poor_1
+		self._db.add_poor_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'])
+		test_file = self.test_file_rich_1
+		self._db.add_rich_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'],
+			test_file['tags'])
 		test_item = self.test_item
+		self._db.add_item(test_item['name'])
+		self._db.add_item_content(test_item['name'], test_item['content_file'])
+		self._db.add_item_tags(test_item['name'], test_item['tags_file'])
+		test_file = self.test_file_poor_2
 		self._db.add_poor_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'])
 		self._db.add_extra_file(test_file['path'], test_item['name'])
-
-	def test_add_extra_file(self):
-		'test that the addition of relationships with extra files works properly'
-		# add the item
-		self._add_extra_file()
-		# initialize the DB connection to check the addition
-		db = sqlite3.connect(self._db_path)
-		cur = db.cursor()
-		# check that the DB contains a single extra file association, as expected
-		cur.execute("SELECT * FROM other_files;")
-		values = cur.fetchall()
-		self.assertTrue(len(values) == 1)
-		# check that file has been properly assigned
-		cur.execute("SELECT file FROM other_files;")
-		value = cur.fetchone()[0]
-		cur.execute("SELECT path FROM files WHERE file_id = %d;" % value)
-		value = cur.fetchone()[0]
-		self.assertTrue(value == self.test_extra_file['file'])
-		# check that item has been properly assigned
-		cur.execute("SELECT item FROM other_files;")
-		value = cur.fetchone()[0]
-		cur.execute("SELECT name FROM items WHERE item_id = %d;" % value)
-		value = cur.fetchone()[0]
-		self.assertTrue(value == self.test_extra_file['item'])
-		# close DB connection and delete it
-		db.close()
-		os.remove(self._db_path)
+		test_file = self.test_file_poor_3
+		self._db.add_poor_file(test_file['path'], test_file['file_checksum'], test_file['content_checksum'])
+		self._db.add_extra_file(test_file['path'], test_item['name'])
+		# test the item get
+		(item_name, content_file, tags_file, extra_files) = self._db.get_item(self.test_item['name'])
+		self.assertTrue(content_file == self.test_item['content_file'])
+		self.assertTrue(tags_file == self.test_item['tags_file'])
+		self.assertTrue(extra_files == [self.test_file_poor_2['path'], self.test_file_poor_3['path']])
 
 if __name__ == '__main__':
 	unittest.main()
