5a9f256337ae6aababac331b2b4a04c401df3269
Silvano Cirujano Cuesta <silvanociru@gmail.com>
2011-07-27 19:55:41 +0200

    temporary backup

--- db_backend.py
+++ db_backend.py
@@ -4,6 +4,7 @@ import os
 import sqlite3
 import re
 import inspect
+import logging
 
 DB_PATH_TEST = os.path.join('/tmp/test_auphorg.db')
 
@@ -50,37 +51,52 @@ SCHEMA_ITEMS_VIEW = 'CREATE VIEW items AS ' + \
 
 # exceptions
 class ApoDbError(ValueError):
-	pass
+	def __init__(self):
+		self._logger = logging.getLogger('AuPhOrg')
 
 class ApoDbDupUniq(ApoDbError):
 	def __init__(self, item_type, field, value):
+		super(ApoDbDupUniq, self).__init__()
 		self.item_type = item_type
 		self.field = field
 		self.value = value
 
 	def __str__(self):
-		return 'there is already a %s with %s = %s' % \
+		err_msg = 'there is already a %s with %s = %s' % \
 			(self.item_type, self.field, self.value)
+		self._logger.error(err_msg)
+		return err_msg
 
 class ApoDbMissingFile(ApoDbError):
 	def __init__(self, missing_file, name):
+		super(ApoDbMissingFile, self).__init__()
 		self.missing_file = missing_file
 		self.name = name
 
 	def __str__(self):
-		return 'trying to associate the non-existing file %s to the item %s!' % \
+		err_msg = 'trying to associate the non-existing file %s to the item %s!' % \
 			(self.missing_file, self.name)
+		self._logger.error(err_msg)
+		return err_msg
 
 #
 class DbConnector:
 	def __init__(self, db_path=''):
 		'connects to DB for saving collection'
+		self._logger = logging.getLogger('AuPhOrg')
 		if db_path == '':
+			if os.path.exists(DB_PATH_TEST):
+				self._logger.warning('test DB already exists in %s, removing it' % DB_PATH_TEST)
+				os.remove(DB_PATH_TEST)
+				self._logger.warning('test DB removed')
 			db_path = DB_PATH_TEST
+		self._logger.info('connecting to DB %s' % db_path)
 		self._db_path = db_path
 		self._photos_db = sqlite3.connect(self._db_path)
 		self._db_curs = self._photos_db.cursor()
+		self._logger.info('connection to DB established')
 		# if database doesn't have the tables yet, create them
+		self._logger.info('adding the tables to the DB')
 		self._db_curs.execute("SELECT name FROM sqlite_master WHERE type='table';")
 		tables = self._db_curs.fetchall()
 		if (not (u'files',) in tables) or (not (u'simple_items',) in tables) or (not (u'tags',) in tables):
@@ -92,12 +108,16 @@ class DbConnector:
 			self._db_curs.execute(SCHEMA_ITEM_INDEX)
 			self._db_curs.execute(SCHEMA_ITEMS_VIEW)
 			self._photos_db.commit()
+		self._logger.info('tables added to the DB')
 
 	def __del__(self):
 		'closes the connection to the DB before destroying the object'
+		self._logger.info('closing connection with DB')
 		self._photos_db.close()
+		self._logger.info('connection with DB closed')
 
 	def _insert_query(self, table, values):
+		self._logger.info('adding an entry to table %s with values %s' % (table, str(values)))
 		field_names = ''
 		field_placeholders = ''
 		query_values = []
@@ -109,9 +129,12 @@ class DbConnector:
 		field_placeholders = field_placeholders[:-2]
 		sql_query = 'INSERT INTO %s (%s) VALUES (%s);' % \
 			(table, field_names, field_placeholders)
+		self._logger.info('entry to table %s successfully added' % table)
 		return (sql_query, query_values)
 
 	def _update_query(self, table, values, element_filters):
+		self._logger.info('updating entry of table %s that matches filter %s with values %s' \
+			% (table, str(element_filters), str(values)))
 		field_names = ''
 		field_placeholders = ''
 		query_values = []
@@ -127,10 +150,12 @@ class DbConnector:
 		field_names = field_names[:-2]
 		sql_query = 'UPDATE %s SET %s WHERE %s;' % \
 			(table, field_names, filter_names)
+		self._logger.info('entry to table %s successfully updated' % table)
 		return (sql_query, query_values)
 
 	def _edit_element(self, table, values, element_filters = None):	
 		'inserts the specified values in table, explicitly reporting duplications'
+		self._logger.info('editing entry of table %s' % table)
 		if (element_filters == None):
 			(sql_query, query_values) = self._insert_query(table, values)
 		else:
@@ -146,28 +171,34 @@ class DbConnector:
 				err_field = dup_field.group(1)
 				item_type = table[:-1]
 				raise ApoDbDupUniq(item_type, err_field, values[err_field])
+		self._logger.info('done editing entry')
 		return self._db_curs.lastrowid
 
 	def add_non_raw_file(self, path, timestamp, file_checksum, content_checksum):
 		'adds a file without metadata to the DB'
+		self._logger.info('adding non RAW file %s', path)
 		self._edit_element('files', {\
 			'path': path, \
 			'timestamp': timestamp, \
 			'file_checksum': file_checksum, \
 			'content_checksum': content_checksum})
+		self._logger.info('non RAW file added')
 
 	def add_raw_file(self, path, timestamp, file_checksum):
 		'adds a file without metadata to the DB'
+		self._logger.info('adding RAW file %s', path)
 		self._edit_element('files', {\
 			'path': path, \
 			'timestamp': timestamp, \
 			'file_checksum': file_checksum})
+		self._logger.info('RAW file added')
 
 	def add_tags(self, model = '', software = '', date_time_original = '',
 						create_date = '', image_width = '', image_height = '',
 						tags_list = '',	hierarchical_subject = '',
 						subject = '', keywords = ''):
 		'adds a tags item to the DB'
+		self._logger.info('adding item tags')
 		rowid = self._edit_element('tags', { \
 			'Model': model, \
 			'Software': software, \
@@ -179,10 +210,12 @@ class DbConnector:
 			'HierarchicalSubject': hierarchical_subject, \
 			'Subject': subject, \
 			'Keywords': keywords})
+		self._logger.info('item tags successfully added')
 		return rowid
 
 	def add_rich_file(self, path, timestamp, file_checksum, image_checksum, tags):
 		'adds a file with metadata to the DB'
+		self._logger.info('adding rich file %s', path)
 		tags_index = self.add_tags(tags['Model'], tags['Software'], 
 										tags['DateTimeOriginal'], tags['CreateDate'], 
 										tags['ImageWidth'], tags['ImageHeight'], 
@@ -194,10 +227,14 @@ class DbConnector:
 			'file_checksum': file_checksum, \
 			'content_checksum': image_checksum, \
 			'tags': tags_index})
+		self._logger.info('rich file added')
 
 	def get_rich_file_tags(self, path):
+		'gets the tags of a rich file'
+		self._logger.info('getting tags of rich file %s', path)
 		self._db_curs.execute('SELECT t.* FROM tags t, files f WHERE t.tags_id = f.tags AND path = ?;', [path])
 		tags_list = self._db_curs.fetchone()
+		print tags_list
 		tags = {}
 		tags['Model'] = tags_list[1]
 		tags['Software'] = tags_list[2]
@@ -209,14 +246,18 @@ class DbConnector:
 		tags['HierarchicalSubject'] = tags_list[8]
 		tags['Subject'] = tags_list[9]
 		tags['Keywords'] = tags_list[10]
+		self._logger.info('tags of rich file obtained')
 		return tags
 
 	def add_item(self, name):
 		'adds a multimedia item to the DB'
+		self._logger.info('adding item %s' % name)
 		self._edit_element('simple_items', {'name': name})
+		self._logger.info('item added')
 
 	def add_item_content(self, name, content_file):
 		'adds a content file to a multimedia item into the DB'
+		self._logger.info('adding to item %s the file %s as its content file' % (name, content_file))
 		cur = self._db_curs
 		cur.execute('SELECT file_id FROM files WHERE path = "%s";' % content_file)
 		try:
@@ -225,12 +266,11 @@ class DbConnector:
 			raise ApoDbMissingFile(content_file, item_name)
 		cur.execute('SELECT content_file FROM items WHERE name = ?;', (name,))
 		self._edit_element('simple_items', {'content_file': content_file_id}, {'name': name})
-#		self._db_curs.execute('UPDATE simple_items SET content_file = ? WHERE name = ?;', (content_file_id, name))
-#		self._photos_db.commit()
-#		return self._db_curs.lastrowid
+		self._logger.info('item added')
 
 	def add_item_tags(self, name, tags_file):
 		'adds a tags file to a multimedia item into the DB'
+		self._logger.info('adding to item %s the file %s as its metadata file' % (name, tags_file))
 		cur = self._db_curs
 		cur.execute('SELECT file_id FROM files WHERE path = "%s";' % tags_file)
 		try:
@@ -239,23 +279,30 @@ class DbConnector:
 			raise ApoDbMissingFile(tags_file, item_name)
 		self._db_curs.execute('UPDATE simple_items SET tags_file = ? WHERE name = ?;', (tags_file_id, name))
 		self._photos_db.commit()
+		self._logger.info('item added')
 		return self._db_curs.lastrowid
 
 	def get_item(self, item_name):
 		'returns the specified item'
+		self._logger.info('getting item %s' % item_name)
 		self._db_curs.execute('SELECT * FROM items WHERE name = ?;', [item_name])
 		try:
 			(item_name, content_file, tags_file, extra_files) = self._db_curs.fetchone()
 		except TypeError, err:
 			if (str(err) == "'NoneType' object is not iterable"):
+				self._db_curs.execute('SELECT * FROM items;')
+				print str(self._db_curs.fetchall())
+				self._logger.warning("item %s doesn't exist yet" % item_name)
 				return None
 			else:
 				raise
 		extra_files = extra_files.split('|')
+		self._logger.info('item obtained')
 		return (item_name, content_file, tags_file, extra_files)
 
 	def add_extra_file(self, file_path, item_name):
 		'adds a relationship with an extra file to the DB'
+		self._logger.info('extending item %s by adding the extra file %s' % (item_name, file_path))
 		cur = self._db_curs
 		cur.execute('SELECT file_id FROM files WHERE path = "%s";' % file_path)
 		try:
@@ -272,3 +319,4 @@ class DbConnector:
 		self._edit_element('other_files', { \
 			'file': file_id, \
 			'item': item_id})
+		self._logger.info('item extended with extra file')
--- files_handler.py
+++ files_handler.py
@@ -4,6 +4,7 @@ import hashlib
 import os.path 
 import db_backend
 import subprocess
+import logging
 
 TAGS_CLASSIFICATION = [
 				'FileName',
@@ -26,19 +27,44 @@ for tag in TAGS_TO_GET:
 	EXIFTOOL_REQUEST = EXIFTOOL_REQUEST + ' -' + tag
 CHECKSUM_TOOL = "/usr/bin/sha512sum"
 
+class ApoFileError(TypeError):
+	def __init__(self):
+		self._logger = logging.getLogger('AuPhOrg')
+
+class ApoFileUnknown(ApoFileError):
+	def __init__(self, filename):
+		super(ApoFileUnknown, self).__init__()
+		self.filename = filename
+		self.fileext = os.path.splitext(filename)[1][1:]
+
+	def __str__(self):
+		self._logger.error('file %s cannot be handled, because its extension (%s) not supported is' % \
+			(self.filename, self.fileext))
+		return 'file %s cannot be handled, because its extension (%s) not supported is' % \
+			(self.filename, self.fileext)
+
 class FilesHandler:
 	def __init__(self):
+		self._logger = logging.getLogger('AuPhOrg')
+		self._logger.info('instanciating DB backend')
 		self._db = db_backend.DbConnector()
+		self._logger.info('DB backend instanciated')
+
+	def __del__(self):
+		self._db = None
 
 	def _file_checksum(self, path):
 		'calculates the SHA512 checksum of the file'
+		self._logger.info('calculating the checksum of the file %s' % path)
 		cmd = CHECKSUM_TOOL + " -b " + path
 		output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
 		lines = output.read().splitlines()
+		self._logger.info('checksum of file calculated')
 		return lines[0].split(' ')[0]
 
 	def _add_raw_image(self, item_name, path):
 		'adds a raw file to the DB'
+		self._logger.info('adding the RAW file %s to the item %s' % (path, item_name))
 		# calculate the checksums of the file
 		file_checksum = self._file_checksum(path)
 		# get the timestamp of the last modification
@@ -46,19 +72,23 @@ class FilesHandler:
 		# add the file to the DB and to the item
 		self._db.add_raw_file(path, file_time, file_checksum)
 		self._db.add_item_content(item_name, path)
+		self._logger.info('RAW file added to item')
 
 	def _image_checksum(self, path):
 		'calculates the SHA512 checksum of the contained image'
+		self._logger.info('calculating the checksum of the image contained in file %s' % path)
 		try:
 			img = Image.open(path)
 			cksm = hashlib.sha512()
 			cksm.update(img.tostring())
+			self._logger.info('checksum of image calculated')
 			return cksm.hexdigest()
 		except Exception, err:
 			print "Error gettig image from file %s: %s" % (path, str(err))
 
 	def _add_jpeg(self, item_name, path):
 		'adds a JPEG file to the DB'
+		self._logger.info('adding the JPEG file %s to the item %s' % (path, item_name))
 		# get the tags of the file
 		exif_tags = {}
 		cmd = EXIFTOOL_REQUEST + ' "' + path + '"'
@@ -74,9 +104,11 @@ class FilesHandler:
 		file_time = os.path.getmtime(path)
 		# add the file to the DB and to the item
 		self._db.add_rich_file(path, file_time, file_checksum, content_checksum, exif_tags)
+		self._logger.info('JPEG file added to item')
 
 	def _add_image(self, item_name, path):
-		'adds a raw file to the DB'
+		'adds a TIFF file to the DB'
+		self._logger.info('adding the TIFF file %s to the item %s' % (path, item_name))
 		# calculate the checksums of the file
 		file_checksum = self._file_checksum(path)
 		content_checksum = self._image_checksum(path)
@@ -84,16 +116,20 @@ class FilesHandler:
 		file_time = os.path.getmtime(path)
 		# add the file to the DB
 		self._db.add_non_raw_file(path, file_time, file_checksum, content_checksum)
+		self._logger.info('TIFF file added to item')
 
 	def _video_checksum(self, path):
 		'calculates the checksum of a video, using ffmpeg and md5sum'
+		self._logger.info('calculating the checksum of the video contained in file %s' % path)
 		cmd = '/usr/bin/ffmpeg -i "' + path + '" -f avi - 2> /dev/null | /usr/bin/sha512sum -b'
 		output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
 		result = output.read().splitlines()[0]
+		self._logger.info('checksum of video calculated')
 		return result.split(' ')[0]
 
 	def _add_video(self, item_name, path):
 		'adds a video file to the DB'
+		self._logger.info('adding the video file %s to the item %s' % (path, item_name))
 		# calculate the checksums of the file
 		file_checksum = self._file_checksum(path)
 		content_checksum = self._video_checksum(path)
@@ -101,19 +137,23 @@ class FilesHandler:
 		file_time = os.path.getmtime(path)
 		# add the file to the DB
 		self._db.add_non_raw_file(path, file_time, file_checksum, content_checksum)
+		self._logger.info('video file added to item')
 
 	def _wav_checksum(self, path):
 		'calculates the checksum of a wave file'
+		self._logger.info('calculating the checksum of the audio contained in file %s' % path)
 		try:
 			wav = wave.open(path, 'rb')
 			cksm = hashlib.sha512()
 			cksm.update(wav.readframes(wav.getnframes()))
+			self._logger.info('checksum of audio calculated')
 			return cksm.hexdigest()
 		except Exception, err:
 			print "Error getting audio from wave file %s: %s" % (path, str(err))
 
 	def _add_audio(self, item_name, path):
 		'adds a video file to the DB'
+		self._logger.info('adding the audio file %s to the item %s' % (path, item_name))
 		# calculate the checksums of the file
 		file_checksum = self._file_checksum(path)
 		content_checksum = self._wav_checksum(path)
@@ -121,9 +161,14 @@ class FilesHandler:
 		file_time = os.path.getmtime(path)
 		# add the file to the DB
 		self._db.add_non_raw_file(path, file_time, file_checksum, content_checksum)
+		self._logger.info('audio file added to item')
+
+	def is_older(self, path):
+		pass
 
 	def add_file(self, path):
 		'adds the file of the given path to the DB'
+		self._logger.info('adding the file %s' % path)
 		# test that path is file
 		if not os.path.isfile(path):
 			raise RuntimeError, "path isn't a file!"
@@ -146,3 +191,6 @@ class FilesHandler:
 			self._add_image(item_name, path)
 		elif (extension in ('.wav')):
 			self._add_audio(item_name, path)
+		else:
+			raise ApoFileUnknown(path)
+		self._logger.info('file added')
