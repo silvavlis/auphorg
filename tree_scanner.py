77b2791d7fb475eb7e1bf9f5e8bd2e9fbc7fc4ff
Silvano Cirujano Cuesta <silvanociru@gmail.com>
2011-09-16 16:24:23 +0200

    First version of multiprocessing working

--- tree_scanner.py
+++ tree_scanner.py
@@ -2,21 +2,78 @@
 import os.path
 import files_handler
 import logging
+import multiprocessing
+
+lock = None
+processed = None
+
+def init_process(inherited_lock, inherited_processed):
+	lock = inherited_lock
+	processed = inherited_processed
+
+def file_processor(filepath):
+	logger = logging.getLogger('AuPhOrg')
+	logger.info('starting process for file %s' % filepath)
+	fsh = files_handler.FilesHandler()
+	print "adding " + filepath
+	fsh.add_file(unicode(filepath, 'utf-8'))
+	print filepath + " added"
+	lock.acquire()
+	processed.value += 1
+	lock.release()
+	print processed.value
+	logger.info('leaving process for file %s' % filepath)
 
 # scans the given tree and tries to add the found files to the DB
-class TreeScanner(list):
-	def __init__(self, photostree_root):
+class TreeScanner():
+	_pool = None
+	def __init__(self):
 		self._logger = logging.getLogger('AuPhOrg')
-		self._logger.info('processing tree %s' % photostree_root)
-		self._fsh = files_handler.FilesHandler()
-		os.path.walk(photostree_root, self._scan_subtree, None)
-		self._logger.info('done processing tree')
+		self._files_to_add = []
 
 	def __del__(self):
-		self._fsh = None
+		self._logger.info('done processing tree')
+
+	def init_pool(self):
+		self.n_cpus = multiprocessing.cpu_count()
+		if TreeScanner._pool != None:
+			raise RuntimeError
+		self._logger.info('starting the pool of processes')
+		TreeScanner._pool = multiprocessing.Pool(self.n_cpus)
+		self._logger.info('pool of processes started')
+
+	def scan_tree(self, photostree_root):
+		self._logger.info('processing tree %s' % photostree_root)
+		os.path.walk(photostree_root, self._scan_subtree, None)
+		n_files_to_add = len(self._files_to_add)
+		print n_files_to_add
+		lock = multiprocessing.Lock()
+		result = TreeScanner._pool.map_async(file_processor, self._files_to_add)
+		result.wait(10)
+		while not result.ready():
+			print "%d out of %d ready (%d%%)" % \
+				(processed.value, n_files_to_add, \
+					100 * processed.value / n_files_to_add)
+			result.wait(10)
 
 	def _scan_subtree(self, arg, dir_path, filenames):
 		for filename in filenames:
 			filepath = os.path.join(dir_path, filename)
 			if not (os.path.isdir(filepath) or os.path.islink(filepath)):
-				self._fsh.add_file(unicode(filepath, 'utf-8'))
+				self._files_to_add.append(filepath)
+
+if __name__ == '__main__':
+	lock = multiprocessing.Lock()
+	processed = multiprocessing.Value('i', 0)
+	log_fh = logging.FileHandler('./auphorg.log')
+	log_so = logging.StreamHandler()
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s: %(message)s')
+	log_fh.setFormatter(formatter)
+	log_so.setFormatter(formatter)
+	logger = logging.getLogger('AuPhOrg')
+	logger.addHandler(log_fh)
+#	logger.addHandler(log_so)
+	logger.setLevel(logging.INFO)
+	tree_scanner = TreeScanner()
+	tree_scanner.init_pool()
+	tree_scanner.scan_tree('/home/silvavlis/tmp/test')
